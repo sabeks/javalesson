fe53fb825b6bb9b83c7e94f96ff061a23c351d02
saba aptsiauri <llamerchik@gmail.com>
2023-08-02 16:15:26 +0400

    nm

---
+++ app.js
@@ -0,0 +1,17 @@
+// task 1
+const button = document.querySelector(`button`);
+button.addEventListener("click", (e) => {
+  button.remove();
+});
+// task 2
+const div = document.querySelector(".container");
+const img = document.querySelector("img");
+image.setAttribute(
+  "src",
+  "https://fastly.picsum.photos/id/405/1400/800.jpg?hmac=4CRI7OpfDWtP4EydVd4Z-1NKSGhBmpZq8OaEXVu3be8"
+);
+img.classList.add("img");
+container.append(img);
+img.setAttribute("alt", "image");
+// task 3
+const character = document.querySelector("#characters-list");
